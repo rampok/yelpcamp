63cf288db1381e799cb568b72f4b7b10eb43224c
Marcel Terlaak <ma.terlaak@gmail.com>
2021-01-02 15:14:07 +0100

    reviews part 1

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const { campgroundSchema } = require('./schemas.js');
 const methodOverride = require('method-override');
 const path = require('path');
 const Campground = require('./models/campground');
+const Review = require('./models/review');
 
 const app = express();
 
@@ -89,6 +90,15 @@ app.delete('/campgrounds/:id', catchAsync( async (req, res) => {
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
 }));
+
+app.post('/campgrounds/:id/reviews', catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+}))
 
 
 // ** Error Handling ** //
--- models/campground.js
+++ models/campground.js
@@ -9,7 +9,11 @@ const CampgroundSchema = new Schema({
     image: String,
     price: Number,
     description: String,
-    location: String
+    location: String,
+    reviews: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Review'
+    }]
 });
 
 

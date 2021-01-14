2ac5b9653a4cbb7a8d7dc6c3323d001fad9670ba
Marcel Terlaak <ma.terlaak@gmail.com>
2021-01-14 18:08:09 +0100

    maps part 1

---
+++ public/javascripts/showPageMap.js
@@ -0,0 +1,19 @@
+const campLocation = campground.geometry.coordinates;
+mapboxgl.accessToken = mapToken;
+
+const map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/mapbox/light-v10',
+center: campLocation,
+zoom: 8
+});
+
+const marker = new mapboxgl.Marker()
+    .setLngLat(campLocation)
+    .setPopup(
+        new mapboxgl.Popup({offset: 25})
+        .setHTML(
+            `<h3>${campground.title}</h3><p>${campground.location}</p>`
+        )
+    )
+    .addTo(map);

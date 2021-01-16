5f5176f94d7e2712fc852566352521ddbb447c67
Marcel Terlaak <ma.terlaak@gmail.com>
2021-01-16 13:36:21 +0100

    add start script

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.use(mongoSanitize());
 
 const store = new MongoDBStore({
     url: dbUrl,
-    secret: secret',
+    secret: secret,
     touchAfter: 24 * 60 *60
 });
 
@@ -167,6 +167,8 @@ app.use((err, req, res, next) => {
 
 
 // ** Server ** //
-app.listen(3000, () => {
-    console.log('Serving on port 3000');
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Serving on port ${port}`);
 });
